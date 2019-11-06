5b4a6d7e5824be858f20156357e0e9cb15aeb9f8
github username <github email address>
2019-11-06 14:19:20 -0500

    merged swal branch

--- src/redux/actionCreator.js
+++ src/redux/actionCreator.js
@@ -1,6 +1,6 @@
 import { confirm } from '../components/Alerts'
 
-const URL = 'https://spaceshare-api.herokuapp.com/api/v1'
+const URL = 'http://localhost:3000'
 const LOGIN = `${URL}/login`
 const PROFILE = `${URL}/profile`
 const SPACES = `${URL}/get_spaces`
